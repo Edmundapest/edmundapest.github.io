6ce9d97e701694b3b79d400ea8bfb12550fc9b86
Edmund <ledmund1996@gmail.com>
2024-07-21 19:12:02 +0800

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
